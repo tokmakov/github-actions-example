86190395476cdff8b417984216837d9c5d6703ac
Evgeniy Tokmakov <tokmakov.e@mail.ru>
2022-06-26 13:48:16 +0300

    create docker action

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,3 +7,4 @@ json='{\"chat_id\":\"%s\",\"text\":\"%s\"}'
 printf -v data "$json" "$INPUT_CHAT" "$INPUT_TEXT"
 
 curl -X POST -H "$header" -d "$data" $api_url > /dev/null 2>&1
+echo 'done entrypoint.sh'
