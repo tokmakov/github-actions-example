001992eefe04904c359394bca20b187e20dbdd32
Evgeniy Tokmakov <tokmakov.e@mail.ru>
2022-06-27 16:49:37 +0300

    fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,18 @@ set -e
 api_url="https://api.telegram.org/bot${INPUT_TOKEN}/sendMessage"
 header='Content-Type: application/json; charset=utf-8'
 
-text='Задание завершилось успешно'
+message='Задание завершилось успешно'
 if [[ $INPUT_RESULT == 'failure' ]]; then
-    text='Задание завершилось ошибкой'
+    message='Задание завершилось ошибкой'
 fi
 if [[ $INPUT_RESULT == 'skipped' ]]; then
-    text='Задание было пропущено'
+    message='Задание было пропущено'
 fi
 if [[ $INPUT_RESULT == 'cancelled' ]]; then
-    text='Задание было отменено'
+    message='Задание было отменено'
 fi
 
 json='{\"chat_id\":\"%s\",\"text\":\"%s\"}'
-printf -v data "$json" "$INPUT_CHAT" "$INPUT_TEXT"
+printf -v data "$json" "$INPUT_CHAT" "$message"
 
 curl -X POST -H "$header" -d "$data" $api_url > /dev/null 2>&1
